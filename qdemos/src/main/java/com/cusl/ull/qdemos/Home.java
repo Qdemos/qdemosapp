822ceea7596d24109e59eb4a43035a87429147b9
Paco Martin Fernandez <erespia2@gmail.com>
2014-03-19 17:21:49 +0000

    Compatibilidad con Android Wear para manejo de notificaciones en dispositivos wearables como smartwatches

--- qdemos/src/main/java/com/cusl/ull/qdemos/Home.java
+++ qdemos/src/main/java/com/cusl/ull/qdemos/Home.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Locale;
 
 import android.app.ActionBar;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
